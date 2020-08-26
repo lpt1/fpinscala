24a442abbba87a2c3bc0b5bea6708105c0e46777
Lee Thompson <leet.104@gmail.com>
2020-08-27 00:22:29 +0100

    Tree exercises

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -124,4 +124,18 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def filterViaFlatMap[A, B](l: List[A])(f: A => Boolean): List[A] = 
     flatMap(l)(a => if (f(a)) List(a) else Nil)
+
+  def addLists(l1: List[Int], l2: List[Int]): List[Int] = 
+    (l1, l2) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addLists(t1, t2))
+    }
+
+  def zipWith[A, B, C](l1: List[A], l2: List[B])(f: (A, B) => C): List[C] = 
+    (l1, l2) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+    }
 }
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,45 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+    def size[A](t: Tree[A]): Int = 
+      t match {
+        case Leaf(_) => 1
+        case Branch(l, r) => 1 + size(l) + size(r)
+      }
 
+    def maximum(t: Tree[Int]): Int = 
+      t match {
+        case Leaf(n) => n
+        case Branch(l, r) => maximum(l).max(maximum(r))
+      }
 
+    def depth[A](t: Tree[A]): Int = 
+      t match {
+        case Leaf(n) => 1
+        case Branch(l, r) => 1 + depth(l).max(depth(r))
+      }
 
+    def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
+      t match {
+        case Leaf(a) => Leaf(f(a))
+        case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+      }
+    
+    def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = 
+      t match {
+        case Leaf(a) => f(a)
+        case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+      }
+    
+    def sizeViaFold[A](t: Tree[A]): Int =
+      fold(t)(_ => 1)((v0, v1) => 1 + v0 + v1)
+
+    def maximumViaFold(t: Tree[Int]): Int =
+      fold(t)(v => v)((v0, v1) => v0.max(v1))
+
+    def depthViaFold[A](t: Tree[A]): Int = 
+      fold(t)(_ => 1)((v0, v1) => 1 + v0.max(v1))
+    
+    def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] =
+      fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
 }
